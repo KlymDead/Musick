ba3e3a99f0fa3387339cbbc1f29610e2e1b57b0e
Klym <klymdesign95@gmail.com>
2022-11-18 14:11:37 +0200

    a little update

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
         MusicStyles song = new PopMusic("BoyWithUke","Sick of U", "2:49");
         MusicStyles song2 = new RockMusic("Suicide Silence","YOLO", "4:02");
         MusicStyles song3 = new ClassicMusic("Toms Mucenieks","Spring Waltz", "5:02");
-        MusicStyles[] arr = new MusicStyles[3];
-        arr[0]=song;
-        arr[1]=song2;
-        arr[2]=song3;
+        MusicStyles[] arr = {song,song2,song3};
         for(MusicStyles list : arr ){
            System.out.println(list.playMusic());
         }
+
+
     }
 }
